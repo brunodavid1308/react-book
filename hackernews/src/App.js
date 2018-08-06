0dad9de7c122770eb6abc1460c2f0a8a34998638
bruno <bruno.ortega@biko2.com>
2018-08-06 15:01:58 +0200

    forms

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -2,17 +2,80 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const user = {
+  nombre: 'Bruno',
+  apellido: 'Ortega',
+}
+const list = [
+  {
+    title:'React',
+    url: 'https://facebook.github.io/react/',
+    author: 'Jordan Walke',
+    num_comments:3,
+    points:4,
+    objectID:0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://github.com/reactjs/redux',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments:2,
+    points:5,
+    objectID:1,
+  },
+];
 class App extends Component {
-  render() {
-    const helloWorld = 'Hola! Bienvenidos al camino para aprender React';
-    const user = {
-      nombre: 'Bruno',
-      apellido: 'ortega'
+
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      list,
+      searchTerm:'',
     };
+    this.onDismiss = this.onDismiss.bind(this); // Se puede omitir si se utilizan las arrow functions
+    this.onSearchChange = this.onSearchChange.bind(this);
+  }
+  
+  onDismiss(id){
+    const isNotId = item => item.objectID !== id;
+    const updatedList = this.state.list.filter(isNotId);
+    this.setState({ list:updatedList });
+    
+  }
+  onSearchChange(event){
+    this.setState({ searchTerm: event.target.value });
+  }
+
+  render() {
     return (
       <div className="App">
-        <h1>{user.nombre} {user.apellido}</h1>
-        <h2>{helloWorld}</h2>
+        <form>
+          <input 
+          type="text"
+          onChange = {this.onSearchChange} 
+          />
+        </form>
+        { 
+          this.state.list.map(item =>
+            <div key= {item.objectID}>
+              <span>
+                <a href = {item.url}>{item.title}</a>
+              </span>
+              <span> {item.author}</span>
+              <span> {item.num_comments}</span>
+              <span> {item.points}</span>
+              <span>
+                <button
+                  onClick = {() => this.onDismiss(item.objectID)}
+                  type = "button"
+                >
+                 Dismiss
+                </button>
+              </span>
+            </div>
+          )
+        }
       </div>
     );
   }
