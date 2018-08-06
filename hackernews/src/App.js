4d732e408b17c9258d136e5f4d69600469fded74
bruno <bruno.ortega@biko2.com>
2018-08-06 10:03:35 +0200

    probando app.js

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -4,15 +4,15 @@ import './App.css';
 
 class App extends Component {
   render() {
+    const helloWorld = 'Hola! Bienvenidos al camino para aprender React';
+    const user = {
+      nombre: 'Bruno',
+      apellido: 'ortega'
+    };
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          Hello, To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>{user.nombre} {user.apellido}</h1>
+        <h2>{helloWorld}</h2>
       </div>
     );
   }
