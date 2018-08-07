8420817021e493a8b671f9588dfa86a7b287827f
bruno <bruno.ortega@biko2.com>
2018-08-07 11:35:33 +0200

    componentes

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -2,10 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const user = {
-  nombre: 'Bruno',
-  apellido: 'Ortega',
-}
+
 const list = [
   {
     title:'React',
@@ -24,6 +21,13 @@ const list = [
     objectID:1,
   },
 ];
+
+function isSearched(searchTerm) {
+  return function (item) {
+    return item.title.toLowerCase().includes(searchTerm.toLowerCase());
+  }
+}
+
 class App extends Component {
 
   constructor(props) {
@@ -48,34 +52,63 @@ class App extends Component {
   }
 
   render() {
+    const {searchTerm,list}=this.state;
     return (
       <div className="App">
-        <form>
-          <input 
-          type="text"
-          onChange = {this.onSearchChange} 
+        <Search 
+          value ={searchTerm}
+          onChange = {this.onSearchChange}
+        >
+          Buscar
+        </Search>
+        <Table 
+        list={list}
+        pattern={searchTerm}
+        onDismiss={this.onDismiss}
+        />
+      </div>
+    );
+  }
+}
+
+class Search extends Component {
+  render(){
+    const { value, onChange, children } = this.props;
+    return(
+      <form>
+          {children} <input 
+            type="text"
+            value={value}
+            onChange = {onChange} 
           />
         </form>
-        { 
-          this.state.list.map(item =>
-            <div key= {item.objectID}>
-              <span>
-                <a href = {item.url}>{item.title}</a>
-              </span>
-              <span> {item.author}</span>
-              <span> {item.num_comments}</span>
-              <span> {item.points}</span>
-              <span>
-                <button
-                  onClick = {() => this.onDismiss(item.objectID)}
-                  type = "button"
-                >
-                 Dismiss
-                </button>
-              </span>
-            </div>
-          )
-        }
+    );
+  }
+}
+
+class Table extends Component {
+  render(){
+    const { list, pattern, onDismiss } = this.props;
+    return (
+      <div>
+        {list.filter(isSearched(pattern)).map(item =>
+          <div key= {item.objectID}>
+            <span>
+              <a href = {item.url}>{item.title}</a>
+            </span>
+            <span> {item.author}</span>
+            <span> {item.num_comments}</span>
+            <span> {item.points}</span>
+            <span>
+              <button
+                onClick = {() => onDismiss(item.objectID)}
+                type = "button"
+              >
+               Dismiss
+              </button>
+            </span>
+          </div>
+        )}
       </div>
     );
   }
